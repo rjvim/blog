f0e792f8209c76d17fd53aa3391a974ef6e97eed
Rajiv Seelam <rajiv@betalectic.com>
2022-05-30 10:37:17 +0530

    Fix build issues

--- components/alert.js
+++ components/alert.js
@@ -1,13 +1,13 @@
-import Container from './container'
-import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
+import Container from './container';
+import cn from 'classnames';
+import { EXAMPLE_PATH } from '../lib/constants';
 
 export default function Alert({ preview }) {
   return (
     <div
       className={cn('border-b', {
         'bg-accent-7 border-accent-7 text-white': preview,
-        'bg-accent-1 border-accent-2': !preview,
+        'bg-accent-1 border-accent-2': !preview
       })}
     >
       <Container>
@@ -15,12 +15,12 @@ export default function Alert({ preview }) {
           {preview ? (
             <>
               This page is a preview.{' '}
-              <a
+              {/* <a
                 href="/api/exit-preview"
                 className="underline hover:text-cyan duration-200 transition-colors"
               >
                 Click here
-              </a>{' '}
+              </a>{' '} */}
               to exit preview mode.
             </>
           ) : (
@@ -38,5 +38,5 @@ export default function Alert({ preview }) {
         </div>
       </Container>
     </div>
-  )
+  );
 }
--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -27,16 +27,18 @@ export default function Post({ post, morePosts, preview }) {
             <article className="mb-32">
               <Head>
                 <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
+                  {post.title} | {CMS_NAME}
                 </title>
                 <meta property="og:image" content={post.ogImage.url} />
               </Head>
-              <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-              />
+              <div className="mx-auto">
+                <PostHeader
+                  title={post.title}
+                  coverImage={post.coverImage}
+                  date={post.date}
+                  author={post.author}
+                />
+              </div>
               <PostBody content={post.content} />
             </article>
           </>

c794ef3c4e6fbd7e12a10feab2a944afe95e87df
Rajiv Seelam <rajiv@betalectic.com>
2022-06-03 14:10:38 +0530

    Add back icon

--- components/header.js
+++ components/header.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
+import { XCircleIcon, ArrowLeftIcon } from '@heroicons/react/solid';
 
 export default function Header() {
   return (
-    <h2 className="text-base font-semibold tracking-tight md:tracking-tighter leading-tight mb-8 mt-8">
+    <h2 className="text-base text-gray-500 font-normal tracking-tight md:tracking-tighter leading-tight mb-8 mt-8">
       <Link href="/">
         <a className="hover:underline tracking-wide uppercase flex items-center">
-          {'< '} Home
+          <ArrowLeftIcon className="w-4 h-4" />
+          <span className="ml-2">Home</span>
         </a>
       </Link>
     </h2>

79bed8ae3463824151b181807d8d1656dc6245f1
Rajiv Seelam <rajiv@betalectic.com>
2022-06-03 14:25:38 +0530

    Add padding

--- components/container.js
+++ components/container.js
@@ -1,7 +1,7 @@
 export default function Container({ children }) {
   return (
     <div>
-      <div className="mx-auto max-w-5xl">{children}</div>
+      <div className="px-4 md:px-0 mx-auto max-w-5xl">{children}</div>
     </div>
   );
 }
